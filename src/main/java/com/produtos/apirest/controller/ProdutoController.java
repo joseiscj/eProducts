aad65c0fde5d97fe50e941ea85c0013153f979bb
joseiscj <jose.ivan.junior@ccc.ufcg.edu.br>
2020-06-26 19:11:56 -0300

    criação de método GET no controller para listar todos os produtos ou um produto individualmente

---
+++ src/main/java/com/produtos/apirest/controller/ProdutoController.java
@@ -0,0 +1,33 @@
+package com.produtos.apirest.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.produtos.apirest.models.Produto;
+import com.produtos.apirest.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping(value="/api")
+public class ProdutoController {
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
+	@GetMapping("/produtos")
+	public List<Produto> listaProdutos() {
+		return produtoRepository.findAll();
+	}
+	
+	@GetMapping("/produtos/{id}")
+	public Produto listaProduto(@PathVariable(value="id") long id) {
+		return produtoRepository.findById(id);
+	}
+	
+	
+
+}
--- src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
+++ src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
@@ -6,5 +6,5 @@ import com.produtos.apirest.models.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 	
-
+	Produto findById(long id);
 }
